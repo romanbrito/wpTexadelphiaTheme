9f219e8abc7c64206b283ec2cd7c538a5a7e256c
roman <romanbrito1@gmail.com>
2018-04-26 14:07:54 -0500

    associative array on locationsInit to avoid error when location is added or removed

--- js/locationsInit.js
+++ js/locationsInit.js
@@ -1,34 +1,19 @@
 var GETinfo = window.location.href.split('/');
 $.getJSON('../wp-content/themes/texsite/json/locations.json', function (data) {
-    console.log(data.locations)
-    console.log(GETinfo[3])
+
+    var arrayToObject  = function arrayToObject(array) {
+        return array.reduce(function (obj, item) {
+            obj[item.id] = item;
+            return obj;
+        }, {})
+    };
+
+    var locationsObject = arrayToObject(data.locations)
+
+
     if (GETinfo[3] === "locations-menu") {
         loadMapsScript(data.locations);
-    } else if (GETinfo[3] === "great-hills") {
-        loadMapsScript([data.locations[0]]);
-    } else if (GETinfo[3] === "lakeline") {
-        loadMapsScript([data.locations[1]]);
-    } else if (GETinfo[3] === "sunset") {
-        loadMapsScript([data.locations[2]]);
-    } else if (GETinfo[3] === "frisco-warren-pkwy") {
-        loadMapsScript([data.locations[3]]);
-    } else if (GETinfo[3] === "las-colinas") {
-        loadMapsScript([data.locations[4]]);
-    } else if (GETinfo[3] === "old-town") {
-        loadMapsScript([data.locations[5]]);
-    } else if (GETinfo[3] === "plano") {
-        loadMapsScript([data.locations[6]]);
-    } else if (GETinfo[3] === "richardson") {
-        loadMapsScript([data.locations[7]]);
-    } else if (GETinfo[3] === "arlington-uta-campus") {
-        loadMapsScript([data.locations[8]]);
-    } else if (GETinfo[3] === "okc") {
-        loadMapsScript([data.locations[9]]);
-    } else if (GETinfo[3] === "laredo") {
-        loadMapsScript([data.locations[10]]);
-    } else if (GETinfo[3] === "mcallen") {
-        loadMapsScript([data.locations[11]]);
-    } else if (GETinfo[3] === "houston-westheimer") {
-        loadMapsScript([data.locations[12]]);
+    } else {
+        loadMapsScript([ locationsObject[GETinfo[3]] ])
     }
 });
