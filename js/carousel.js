ce7ed2f671bc833071abe72005a512ff0ed9d6cb
Roman Brito <romanbrito1@gmail.com>
2018-10-14 10:38:49 -0500

    working on manual slideshow

--- js/carousel.js
+++ js/carousel.js
@@ -11,23 +11,27 @@
     const FIGURE = document.querySelectorAll('.slide');
     const DURATION = 7000; // milliseconds
 
+    let state = 0;
+
     for (let i = 0; i < CHEVRON_LEFT.length; i++) {
         CHEVRON_LEFT[i].addEventListener('click', function (e) {
-            console.log(e.currentTarget.parentNode.getAttribute('data-index-number'));
+            state < FIGURE.length-1 ? state+=1: state=0;
+            slide(state);
         });
 
     }
 
     for (let i = 0; i < CHEVRON_RIGHT.length; i++) {
         CHEVRON_RIGHT[i].addEventListener('click', function (e) {
-            console.log(e.currentTarget.parentNode.getAttribute('data-index-number'));
+            state > 0 ? state-=1: state=FIGURE.length-1;
+            slideRight(state);
         });
     }
 
-    slideShow();
+    slide(0);
 
     function slide(idx) {
-        setTimeout(function () {
+
             FIGURE[idx].classList.add('active');
 
             if (idx < 2) {
@@ -45,7 +49,30 @@
                 FIGURE[idx - 1].classList.add('next');
                 FIGURE[idx - 2].classList.remove('active', 'next');
             }
-        }, idx * DURATION)
+    }
+
+    function slideRight(idx) {
+
+        console.log('right idx ' + idx);
+
+
+        FIGURE[idx].classList.remove('next', 'right');
+
+        // if (idx < 2) {
+            // case idx 0
+            // if (idx < 1) {
+            //     FIGURE[FIGURE.length - 1].classList.add('next');
+            //     FIGURE[FIGURE.length - 2].classList.remove('active', 'next');
+            // } else {
+                // case idx 1
+                // FIGURE[idx - 1].classList.add('next');
+                // FIGURE[FIGURE.length - 1].classList.remove('active', 'next');
+            // }
+        // } else {
+            // case > 1
+            FIGURE[idx + 1].classList.add('right');
+            FIGURE[idx + 2].classList.remove('active', 'next');
+        // }
     }
 
     function slideShow() {
