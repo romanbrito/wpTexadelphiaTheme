529de59e0b7e6d8c4e951e1639475f54a0878bf4
Roman Brito <romanbrito1@gmail.com>
2018-10-15 13:34:54 -0500

    manaual sliding left or right independently

--- js/carousel.js
+++ js/carousel.js
@@ -17,38 +17,62 @@
     function left(cb) {
         for (let i = 0; i < LENGTH; i++) {
             FIGURE[i].style.left = '100%';
-            cb();
         }
+        cb();
+    }
+
+    function right(cb) {
+        for (let i = 0; i < LENGTH; i++) {
+            FIGURE[i].style.left = '-100%';
+        }
+        cb();
     }
 
     for (let i = 0; i < CHEVRON_LEFT.length; i++) {
         CHEVRON_LEFT[i].addEventListener('click', function (e) {
-            state < LENGTH-1 ? state+=1: state=0;
-            left(function(){slide(state)});
+            state < LENGTH - 1 ? state += 1 : state = 0;
+            left(function () {
+                slide(state)
+            });
         });
 
     }
 
     for (let i = 0; i < CHEVRON_RIGHT.length; i++) {
         CHEVRON_RIGHT[i].addEventListener('click', function (e) {
-            state > 0 ? state-=1: state=LENGTH-1;
-            slide(state);
+            console.log('calling event handler');
+            state > 0 ? state -= 1 : state = LENGTH - 1;
+            right(function () {
+                slideRight(state)
+            });
         });
     }
 
-    slide(0);
+    //initial slide
+    left(function () {
+        slide(0);
+    });
+    // right(function () {
+    //     slideRight(0);
+    // });
 
     // Returns the correct index to avoid negative or above last index
     function normalize(idx) {
-        if(idx < 0) return LENGTH + idx;
-        if(idx > LENGTH - 1) return LENGTH - idx;
+        if (idx < 0) return LENGTH + idx;
+        if (idx > LENGTH - 1) return idx - LENGTH;
         return idx;
     }
 
     function slide(idx) {
-            FIGURE[idx].classList.add('active');
-            FIGURE[normalize(idx - 1)].classList.add('next');
-            FIGURE[normalize(idx - 2)].classList.remove('active', 'next');
+        FIGURE[idx].classList.add('active');
+        FIGURE[normalize(idx - 1)].classList.add('next');
+        FIGURE[normalize(idx - 2)].classList.remove('active', 'next');
+    }
+
+    function slideRight(idx) {
+        FIGURE[idx].classList.add('active_right');
+        FIGURE[normalize(idx + 1)].classList.add('next_right');
+        FIGURE[normalize(idx + 2)].classList.remove('active_right', 'next_right');
     }
 
 })();
