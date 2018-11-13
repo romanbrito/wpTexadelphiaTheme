5918fe282c05bda85e9deca7bd9cb0e052d5c0d5
Roman Brito <romanbrito1@gmail.com>
2018-11-13 16:15:35 -0600

    adding close button to menu

--- js/nav.js
+++ js/nav.js
@@ -1,5 +1,32 @@
 var BUTTON = document.querySelector('.menu-toggle');
 var MENU_MAIN_NAVIGATION = document.querySelector('.menu-main-navigation-container');
+var CLOSE_BUTTON = document.createElement('BUTTON');
+CLOSE_BUTTON.innerHTML = 'X';
+CLOSE_BUTTON.classList.add('close-button');
+MENU_MAIN_NAVIGATION.appendChild(CLOSE_BUTTON);
+
 BUTTON.addEventListener('click', function (e) {
-  MENU_MAIN_NAVIGATION.classList.toggle('show-menu');
+ showButton();
+ showMenu();
 });
+
+CLOSE_BUTTON.addEventListener('click', function (e) {
+  showButton();
+  showMenu();
+});
+
+function showMenu() {
+  if ( MENU_MAIN_NAVIGATION.style.display === 'flex') {
+    MENU_MAIN_NAVIGATION.style.display= 'none';
+  } else {
+    MENU_MAIN_NAVIGATION.style.display= 'flex';
+  }
+}
+
+function showButton() {
+  if ( BUTTON.style.display !== 'none') {
+    BUTTON.style.display= 'none';
+  } else {
+    BUTTON.style.display= 'block';
+  }
+}
