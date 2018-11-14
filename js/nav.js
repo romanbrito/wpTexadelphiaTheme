dec6e2a6c57d273d6e0d61f0d16f4be942d773ab
Roman Brito <romanbrito1@gmail.com>
2018-11-14 10:27:49 -0600

    some navigation adjustments and animation

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,11 @@
 var BUTTON = document.querySelector('.menu-toggle');
 var MENU_MAIN_NAVIGATION = document.querySelector('.menu-main-navigation-container');
+var FRONTPAGE_ARTICLE = document.querySelector('.frontpage-article');
 var CLOSE_BUTTON = document.createElement('BUTTON');
 CLOSE_BUTTON.innerHTML = 'X';
 CLOSE_BUTTON.classList.add('close-button');
 MENU_MAIN_NAVIGATION.appendChild(CLOSE_BUTTON);
+
 
 BUTTON.addEventListener('click', function (e) {
  showButton();
@@ -18,8 +20,12 @@ CLOSE_BUTTON.addEventListener('click', function (e) {
 function showMenu() {
   if ( MENU_MAIN_NAVIGATION.style.display === 'flex') {
     MENU_MAIN_NAVIGATION.style.display= 'none';
+    MENU_MAIN_NAVIGATION.classList.toggle('show-menu');
+    FRONTPAGE_ARTICLE.classList.toggle('show-frontpage-article');
   } else {
     MENU_MAIN_NAVIGATION.style.display= 'flex';
+    MENU_MAIN_NAVIGATION.classList.toggle('show-menu');
+    FRONTPAGE_ARTICLE.classList.toggle('show-frontpage-article');
   }
 }
 
