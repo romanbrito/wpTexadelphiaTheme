6b22f762e979d3c1f090b7eea0e7fa218540102d
Roman Brito <romanbrito1@gmail.com>
2018-11-21 16:38:06 -0600

    direction button functionality

--- locations/src/components/Menus.js
+++ locations/src/components/Menus.js
@@ -2,11 +2,14 @@ import React from 'react'
 import {destination} from '../utilities/utilities'
 import {MenuContainer, MenuButton} from './StyledMenus'
 
-const Menus = props =>
+const Menus = ({list}) =>
   <MenuContainer>
     <MenuButton>Menu</MenuButton>
     <MenuButton>Catering Menu</MenuButton>
-    <MenuButton directions>Directions</MenuButton>
+    <MenuButton directions
+                onClick={e => window.open(destination(list.address, list.city, list.state, list.zip))}>
+      Directions
+    </MenuButton>
   </MenuContainer>
 
 export default Menus
