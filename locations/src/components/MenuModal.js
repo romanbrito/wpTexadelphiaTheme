e9e15204a737101107b1d77c4368695ba764cbfb
Roman Brito <romanbrito1@gmail.com>
2018-11-22 13:45:11 -0600

    showing pdf in larger screens or images otherwise or if objects not supported

--- locations/src/components/MenuModal.js
+++ locations/src/components/MenuModal.js
@@ -17,16 +17,32 @@ const MenuModal = ({list, menuType, displayMenu}) =>
             <object
               data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/House_' + list.label + '.pdf'}
               type="application/pdf" width="100%" height="100%">
-              Your browser does not support objects
+
+              {/*Your browser does not support objects*/}
+              {list.houseMenuUrl.map(url =>
+              <img key={url} src={'https://www.texadelphia.com/wp-content/themes/texsite/images/' + url + '.jpg'} alt="menu"
+                   width="100%"/>)}
+
             </object>
             :
             <object
               data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/Catering_' + list.label + '.pdf'}
               type="application/pdf" width="100%" height="100%">
-              Your browser does not support objects
+
+              {/*Your browser does not support objects*/}
+              {list.cateringMenuUrl.map(url =>
+                <img key={url} src={'https://www.texadelphia.com/wp-content/themes/texsite/images/' + url + '.jpg'} alt="menu"
+                     width="100%"/>)}
             </object>
           :
-          <div>small</div>
+          menuType === 'house' ?
+            list.houseMenuUrl.map(url =>
+              <img key={url} src={'https://www.texadelphia.com/wp-content/themes/texsite/images/' + url + '.jpg'} alt="menu"
+                   width="100%"/>)
+            :
+            list.cateringMenuUrl.map(url =>
+              <img key={url} src={'https://www.texadelphia.com/wp-content/themes/texsite/images/' + url + '.jpg'} alt="menu"
+                   width="100%"/>)
         }
       </ModalBody>
     </Modal>
