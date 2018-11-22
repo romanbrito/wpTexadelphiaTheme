f447499608fd79331d1cd466dd0d07bccd3f57c4
Roman Brito <romanbrito1@gmail.com>
2018-11-22 12:47:44 -0600

    showing pdf menu only on big screens

--- locations/src/components/MenuModal.js
+++ locations/src/components/MenuModal.js
@@ -12,17 +12,21 @@ const MenuModal = ({list, menuType, displayMenu}) =>
         </button>
       </ModalHeader>
       <ModalBody>
-        {menuType === 'house' ?
-          <object
-            data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/House_' + list.label + '.pdf'}
-            type="application/pdf" width="100%" height="100%">
-            Your browser does not support objects
-          </object>:
-          <object
-            data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/Catering_' + list.label + '.pdf'}
-            type="application/pdf" width="100%" height="100%">
-            Your browser does not support objects
-          </object>
+        {window.innerWidth > 900 ?
+          menuType === 'house' ?
+            <object
+              data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/House_' + list.label + '.pdf'}
+              type="application/pdf" width="100%" height="100%">
+              Your browser does not support objects
+            </object>
+            :
+            <object
+              data={'https://www.texadelphia.com/wp-content/themes/texsite/pdf/Catering_' + list.label + '.pdf'}
+              type="application/pdf" width="100%" height="100%">
+              Your browser does not support objects
+            </object>
+          :
+          <div>small</div>
         }
       </ModalBody>
     </Modal>
